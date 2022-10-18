1d689956bd5d6f96788b0d66cef73884415ab41c
dlswns6532 <hij092919@gmail.com>
2022-10-18 12:17:49 +0900

    TodoApp onToggle event test ok

--- src/TodoApp_gitInJun.js
+++ src/TodoApp_gitInJun.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
-
+import React, { useCallback, useRef, useState } from "react";
+import TodoForm from "./TodoForm"
+import TodoList from "./TodoList"
 const TodoApp_gitInJun = () => {
     const [todos, setTodos] = useState([
         {
@@ -15,13 +14,25 @@ const TodoApp_gitInJun = () => {
             done: true,
         }
     ]);
-
-    return (
-        <div>
-            <TodoForm />
-            <TodoList todos={todos} />
-        </div>
-    );
+    const nextId = useRef(3);
+    const onInsert = useCallback(text => {
+        setTodos(
+            todos.concat({
+                id: nextId.current,
+                text,
+                done: false,
+            })
+        );
+        nextId.current += 1;
+    }, [todos]);
+    const onToggle = useCallback(id => {
+        setTodos(
+            todos.map(todo => todo.id === id ? {...todo, done: !todo.done} : todo)
+        );
+    }, [todos]);
+    return (<div>
+    <TodoForm onInsert={onInsert}/>
+    <TodoList todos={todos} onToggle={onToggle} />
+    </div>);
 };
-
 export default TodoApp_gitInJun;
--- src/TodoApp_gitInJun.test.js
+++ src/TodoApp_gitInJun.test.js
@@ -13,4 +13,19 @@ describe('<TodoApp_gitInJun />', () => {
         getByText('TDD 배우기');
         getByText('react-testing-library');
     });
+    it('creates new todo', () => {
+        const { getByPlaceholderText, getByText } = render(<TodoApp_gitInJun />);
+        fireEvent.change(getByPlaceholderText('할 일을 입력하세요'),
+        { target: { value: "새 항목 추가하기" } }
+        );
+        fireEvent.click(getByText('등록'));
+        getByText('새 항목 추가하기');
+    });
+    it('toggles todo', () => {
+        const { getByText } = render(<TodoApp_gitInJun />);
+        const todoText = getByText('TDD 배우기');
+        expect(todoText).not.toHaveStyle('text-decoration: line-through');
+        fireEvent.click(todoText);
+        expect(todoText).toHaveStyle('text-decoration: line-through');
+    });
 });
