62062958200a998a7df529ccffab804f613e5673
dlswns6532 <hij092919@gmail.com>
2022-10-18 11:31:57 +0900

    TodoApp TodoForm TodoList lender test ok

--- src/TodoApp_gitInJun.test.js
+++ src/TodoApp_gitInJun.test.js
@@ -8,4 +8,9 @@ describe('<TodoApp_gitInJun />', () => {
         getByText('등록'); // TodoForm 화면에 있는지 확인
         getByTestId('TodoList'); // TodoList 화면에 있는지 확인
     });
+    it('renders two default todos', () => {
+        const { getByText } = render(<TodoApp_gitInJun />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    });
 });
