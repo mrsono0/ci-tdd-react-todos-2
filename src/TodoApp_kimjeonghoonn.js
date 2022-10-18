78c7b3eb8d4dd075ef5489c79d931c07c872f7a3
kimjeonghoonn <jhun6241@daum.net>
2022-10-18 12:18:18 +0900

    TodoApp onInsert onToggle event test ok

--- src/TodoApp_kimjeonghoonn.js
+++ src/TodoApp_kimjeonghoonn.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 
 const TodoApp_kimjeonghoonn = () => {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: 'TDD 배우기',
+            done: false,
+        },
+        {
+            id: 2,
+            text: 'react-testing-library',
+            done: true,
+        }
+    ]);
+    const nextId = useRef(3);
+    const onInsert = useCallback(text => {
+        setTodos(
+            todos.concat({
+                id: nextId.current,
+                text,
+                done: false,
+            })
+        );
+        nextId.current +=1;
+    }, [todos]);
+    const onToggle = useCallback(id => {
+        setTodos(
+            todos.map(todo => 
+                todo.id === id ? {...todo, done: !todo.done} : todo
+            )
+        );
+    }, [todos]);
     return (<div>
-        <TodoForm />
-        <TodoList todos={[]}/>
+        <TodoForm onInsert={onInsert}/>
+        <TodoList todos={todos} onToggle={onToggle} />
     </div>);
 };
 export default TodoApp_kimjeonghoonn;
--- src/TodoApp_kimjeonghoonn.test.js
+++ src/TodoApp_kimjeonghoonn.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, getByText} from '@testing-library/react'
 import TodoApp_kimjeonghoonn from "./Todoapp_kimjeonghoonn";
 
 describe('<TodoApp_kimjeonghoonn />', () => {
@@ -7,5 +7,25 @@ describe('<TodoApp_kimjeonghoonn />', () => {
         const {getByText, getByTestId} = render(<TodoApp_kimjeonghoonn />);
         getByText('등록'); // TodoForm 화면에 있는지 확인
         getByTestId('TodoList'); // TodoList 화면에 있는지 확인
+    });
+    it('renders two defaults todos', () => {
+        const {getByText} = render(<TodoApp_kimjeonghoonn />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    });
+    it('creates new todo', () => {
+        const { getByPlaceholderText, getByText } = render(<TodoApp_kimjeonghoonn />);
+        fireEvent.change(getByPlaceholderText('할 일을 입력하세요'),
+        { target: {value: "새 항목 추가하기"}}
+        );
+        fireEvent.click(getByText('등록'));
+        getByText('새 항목 추가하기');
+    });
+    it('toggles todo', () => {
+        const {getByText} = render(<TodoApp_kimjeonghoonn />);
+        const todoText = getByText('TDD 배우기');
+        expect(todoText).not.toHaveStyle('text-decoration: line-through');
+        fireEvent.click(todoText);
+        expect(todoText).toHaveStyle('text-decoration: line-through');
     });
 })
